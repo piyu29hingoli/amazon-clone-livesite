d36b5c324aea33a825380aff8f88c936ccbd7952
Priyanka Gaikwad <priyanka29hingoli@gmail.com>
2023-07-09 04:06:45 +0100

    task

---
+++ app.js
@@ -0,0 +1,68 @@
+let express = require('express');
+let app = express();
+let port = process.env.PORT||9120;
+let Mongo = require('mongodb');
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+let {dbConnect,getData} = require('./controller/dbcontroller')
+
+// middleware supporting liberary to postman
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cors())
+
+app.get('/',(req,res) => {
+    res.send('Hiii From express')
+})
+
+// get all category
+app.get('/categories',async (req,res)=>{
+    let query = {};
+    let collection = "categories"
+    let output = await getData(collection,query)
+    res.send(output)
+})
+
+// get all product
+app.get('/products',async (req,res)=>{
+    let query = {};
+    let collection = "products"
+    let output = await getData(collection,query)
+    res.send(output)
+})
+
+// similar product
+
+app.get('/SimilarItem', async(req,res) => {
+    let query = {}
+    if(req.query.Ca){
+        query={category_id: Number(req.query.categoryid)}
+    }else{
+        query = {}
+    }
+    let collection = "SimilarItem";
+    let output = await getData(collection,query);
+    res.send(output)
+})
+  
+// details product
+
+app.get('/Details', async(req,res) => {
+    let query = {}
+    if(req.query.SimilarItemid){
+        query={SimilarItem_id: Number(req.query.SimilarItemid)}
+    }else{
+        query = {}
+    }
+    let collection = "Details";
+    let output = await getData(collection,query);
+    res.send(output)
+})
+  
+
+
+app.listen(port,(err) => {
+    dbConnect()
+    if(err) throw err;
+    console.log(`server is running on port ${port}`)
+})
